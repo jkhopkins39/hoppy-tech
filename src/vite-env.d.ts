2188398c9566e850f1cd868a76ede8d5d7ffba59
jkhopkins39 <jeremyyhop@gmail.com>
2026-06-10 22:33:19 -0400

    vercel fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
   readonly VITE_ADMIN_EMAIL?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
 }
 
 declare global {
