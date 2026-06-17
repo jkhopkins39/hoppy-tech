7ebeec81345b02a8420e28241b62f72f5e91b208
jkhopkins39 <jeremyyhop@gmail.com>
2026-06-17 15:54:26 -0400

    Updated portal and supabase auth

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL?: string;
-  readonly SUPABASE_PUBLISHABLE_KEY?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
   readonly VITE_ADMIN_EMAIL?: string;
   readonly VITE_WEB3FORMS_ACCESS_KEY?: string;
 }
